601bfaa32e5a95930f5556d2102246d8858f59a3
kebo <bogdan.parubok@hotmail.com>
2019-11-11 16:20:19 +0200

    hamstering

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hamster
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hamster will die soon..... OUH YEEEEEEŠŠŠŠŠŠŠŠŠ :D");
+        }
+    }
+}
